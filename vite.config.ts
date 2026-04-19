769f9960026d26b317b409daa2d1a0869925695c
Chauge01 <danny359131763@gmail.com>
2026-04-20 00:40:22 +0800

    chore: prepare github pages publishing

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/reptile-love-plus-mock/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
